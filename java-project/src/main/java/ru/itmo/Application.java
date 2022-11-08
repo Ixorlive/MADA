ceba5c39ed28b654f8f054d9a37bc3f049fae268
Dinar Shakurov <dinar5d33@gmail.com>
2022-11-08 21:49:17 +0300

    add java server

--- java-project/src/main/java/ru/itmo/Application.java
+++ java-project/src/main/java/ru/itmo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaProjectApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaProjectApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
