847a78332e0e3f16786c3da0691d46831ac42af0
Dinar Shakurov <dinar5d33@gmail.com>
2022-11-09 13:15:57 +0300

    🤡

--- java-project/src/main/java/ru/itmo/controller/SeleniumController.java
+++ java-project/src/main/java/ru/itmo/controller/SeleniumController.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import ru.itmo.dto.ResponseDto;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -44,13 +46,28 @@ public class SeleniumController {
             idInputLc.clear();
             idInputLc.sendKeys(data.getAccount());
 
-            byte[] captcha = driver.findElement(By.id("m-login-meter-captcha-image")).getScreenshotAs(
-                    OutputType.BYTES);
+            byte[] captcha = driver.findElement(By.id("m-login-meter-captcha-image"))
+                    .getScreenshotAs(OutputType.BYTES);
             //для теста
 //            BufferedImage img = ImageIO.read(new ByteArrayInputStream(captcha));
             return ResponseDto.builder().success(true).captcha(captcha).build();
         } catch (Exception e) {
             return ResponseDto.builder().build();
+        }
+    }
+
+    @GetMapping("/districts")
+    public List<String> getDistricts() {
+        try {
+            driver.get(property.getPage());
+
+            return new Select(driver.findElement(By.id("id_select_region")))
+                    .getOptions()
+                    .stream()
+                    .map(WebElement::getText)
+                    .toList();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
