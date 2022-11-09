fa25523846810545fd4b284abf3e32c94f8206c6
Dinar Shakurov <dinar5d33@gmail.com>
2022-11-09 21:47:13 +0300

    🤡чуть-чуть изменил api

---
+++ java-project/src/main/java/ru/itmo/dto/CaptchaDto.java
@@ -0,0 +1,14 @@
+package ru.itmo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CaptchaDto {
+    private String value;
+}
