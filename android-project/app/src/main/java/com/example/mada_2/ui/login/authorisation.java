d1e139d729f224457130708fb65e10100f80f093
ixorlive <igorshakirov43@gmail.com>
2022-11-10 11:54:50 +0300

    dialog captcha

--- android-project/app/src/main/java/com/example/mada_2/ui/login/authorisation.java
+++ android-project/app/src/main/java/com/example/mada_2/ui/login/authorisation.java
@@ -2,13 +2,12 @@ package com.example.mada_2.ui.login;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AlertDialogLayout;
 import androidx.fragment.app.*;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.Intent;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,23 +17,15 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.mada_2.R;
-import com.example.mada_2.server_connection.service.BaseSource;
-import com.example.mada_2.server_connection.service.HttpBaseSource;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import kotlin.jvm.JvmOverloads;
 
 public class authorisation extends Fragment {
 
 
     List<String> dist = null;
 
-    public authorisation(List<String> dist)
-    {
+    public authorisation(List<String> dist) {
         this.dist = dist;
     }
 
@@ -53,9 +44,36 @@ public class authorisation extends Fragment {
         spinner.setAdapter(adapter);
         Button button = view.findViewById(R.id.login);
         button.setOnClickListener(v -> {
+            Dialog d = createDialog();
+            d.show();
 //            HttpBaseSource.Companion.getClient().authorizeAsync(view.findViewById(R.id.area).getText(), view.findViewById(R.id.password).getText());
         });
         return view;
     }
 
+    public Dialog createDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_captcha, null);
+//        ImageView a = view.findViewById(R.id.img_captcha);
+        //a.setBackground(getResources().getDrawable(R.drawable.buttons_gradient));
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(view)
+                // Add action buttons
+                .setPositiveButton("enter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
+        return builder.create();
+    }
 }
