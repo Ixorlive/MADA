cb84da44d28d418c96bddab81015a479bcc7bdfd
Vasya <khitrik.rozanova.svetlana@gmail.com>
2022-11-10 11:40:51 +0300

    fix: исправлено открытие авторизации

--- android-project/app/src/main/java/com/example/mada_2/MainActivity.java
+++ android-project/app/src/main/java/com/example/mada_2/MainActivity.java
@@ -10,22 +10,35 @@ import android.preference.PreferenceManager;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.mada_2.server_connection.service.HttpBaseSource;
 import com.example.mada_2.ui.login.authorisation;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.fragment_authorisation);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.fragment_authorisation);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-
+        List<String> dist = null;
+        try {
+            dist = HttpBaseSource.Companion.getClient().getDistrictsAsync().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Fragment auth = new authorisation(dist);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+                auth).commit();
 //        Fragment main_camera = new authorisation();
 //        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
 //                main_camera).commit();
